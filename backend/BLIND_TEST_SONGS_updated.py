f97541a82bf324e95eaaef570845ef26ca879239
AMSChopper <theo.buignet2@gmail.com>
2026-04-15 22:35:35 +0200

    Add files via upload

---
+++ backend/BLIND_TEST_SONGS_updated.py
@@ -0,0 +1,108 @@
+# Remplacer la liste BLIND_TEST_SONGS dans server.py par celle-ci :
+
+BLIND_TEST_SONGS = [
+    # ===== CLASSIQUES INTEMPORELS =====
+    {"title": "Blinding Lights", "artist": "The Weeknd", "youtube_id": "4NRXx6U8ABQ"},
+    {"title": "Shape of You", "artist": "Ed Sheeran", "youtube_id": "JGwWNGJdvx8"},
+    {"title": "Dance Monkey", "artist": "Tones and I", "youtube_id": "q0hyYWKXF0Q"},
+    {"title": "Someone Like You", "artist": "Adele", "youtube_id": "hLQl3WQQoQ0"},
+    {"title": "Rolling in the Deep", "artist": "Adele", "youtube_id": "rYEDA3JcQqw"},
+    {"title": "Uptown Funk", "artist": "Mark Ronson ft. Bruno Mars", "youtube_id": "OPf0YbXqDm0"},
+    {"title": "Despacito", "artist": "Luis Fonsi ft. Daddy Yankee", "youtube_id": "kJQP7kiw5Fk"},
+    {"title": "See You Again", "artist": "Wiz Khalifa ft. Charlie Puth", "youtube_id": "RgKAFK5djSk"},
+    {"title": "Gangnam Style", "artist": "PSY", "youtube_id": "9bZkp7q19f0"},
+    {"title": "Hello", "artist": "Adele", "youtube_id": "YQHsXMglC9A"},
+    {"title": "Thinking Out Loud", "artist": "Ed Sheeran", "youtube_id": "lp-EO5I60KA"},
+    {"title": "Counting Stars", "artist": "OneRepublic", "youtube_id": "hT_nvWreIhg"},
+    {"title": "Roar", "artist": "Katy Perry", "youtube_id": "CevxZvSJLk8"},
+    {"title": "Happy", "artist": "Pharrell Williams", "youtube_id": "ZbZSe6N_BXs"},
+    {"title": "Radioactive", "artist": "Imagine Dragons", "youtube_id": "ktvTqknDobU"},
+    {"title": "Stay", "artist": "The Kid LAROI & Justin Bieber", "youtube_id": "kTJczUoc26U"},
+    {"title": "Levitating", "artist": "Dua Lipa", "youtube_id": "TUVcZfQe-Kw"},
+    {"title": "drivers license", "artist": "Olivia Rodrigo", "youtube_id": "ZmDBbnmKpqQ"},
+    {"title": "Bad Guy", "artist": "Billie Eilish", "youtube_id": "DyDfgMOUjCI"},
+    {"title": "Old Town Road", "artist": "Lil Nas X ft. Billy Ray Cyrus", "youtube_id": "w2Ov5jzm3j8"},
+    {"title": "Señorita", "artist": "Shawn Mendes & Camila Cabello", "youtube_id": "Pkh8UtuejGw"},
+    {"title": "Get Lucky", "artist": "Daft Punk ft. Pharrell Williams", "youtube_id": "5NV6Rdv1a3I"},
+    {"title": "Stressed Out", "artist": "Twenty One Pilots", "youtube_id": "pXRviuL6vMY"},
+    {"title": "Shallow", "artist": "Lady Gaga & Bradley Cooper", "youtube_id": "bo_efYhYU2A"},
+    {"title": "Closer", "artist": "The Chainsmokers ft. Halsey", "youtube_id": "PT2_F-1esPk"},
+    {"title": "Love Yourself", "artist": "Justin Bieber", "youtube_id": "oyEuk8j8imI"},
+    {"title": "Can't Stop the Feeling!", "artist": "Justin Timberlake", "youtube_id": "ru0K8uYEZWw"},
+    {"title": "Havana", "artist": "Camila Cabello ft. Young Thug", "youtube_id": "BQ0mxQXmLsk"},
+    {"title": "Perfect", "artist": "Ed Sheeran", "youtube_id": "2Vv-BfVoq4g"},
+    {"title": "Believer", "artist": "Imagine Dragons", "youtube_id": "7wtfhZwyrcc"},
+    # ===== CLASSIQUES ANNÉE PAR ANNÉE =====
+    # 2010
+    {"title": "Tik Tok", "artist": "Kesha", "youtube_id": "iP6XpLQM2Cs"},
+    {"title": "California Gurls", "artist": "Katy Perry ft. Snoop Dogg", "youtube_id": "F57P9C4SAW4"},
+    {"title": "We Found Love", "artist": "Rihanna ft. Calvin Harris", "youtube_id": "tg00YEETFzg"},
+    {"title": "Nothin' on You", "artist": "B.o.B ft. Bruno Mars", "youtube_id": "8PTDv_szmL0"},
+    {"title": "Only Girl (In the World)", "artist": "Rihanna", "youtube_id": "pa14VNsdSYM"},
+    # 2011
+    {"title": "Party Rock Anthem", "artist": "LMFAO ft. Lauren Bennett & GoonRock", "youtube_id": "KQ6zr6kCPj8"},
+    {"title": "Somebody That I Used to Know", "artist": "Gotye ft. Kimbra", "youtube_id": "8UVNT4wvIGY"},
+    {"title": "We Are Young", "artist": "fun. ft. Janelle Monáe", "youtube_id": "Sv6dMFF_oz4"},
+    # 2012
+    {"title": "Thrift Shop", "artist": "Macklemore & Ryan Lewis ft. Wanz", "youtube_id": "QK8mJJJvaes"},
+    {"title": "Call Me Maybe", "artist": "Carly Rae Jepsen", "youtube_id": "fWNaR-rxAic"},
+    {"title": "Diamonds", "artist": "Rihanna", "youtube_id": "lWA2pjMjpBs"},
+    # 2013
+    {"title": "Blurred Lines", "artist": "Robin Thicke ft. T.I. & Pharrell", "youtube_id": "yyDUC1LUXSU"},
+    {"title": "Get Lucky", "artist": "Daft Punk ft. Pharrell Williams", "youtube_id": "5NV6Rdv1a3I"},
+    {"title": "Royals", "artist": "Lorde", "youtube_id": "nlcIKh6sBtc"},
+    # 2014
+    {"title": "Happy", "artist": "Pharrell Williams", "youtube_id": "ZbZSe6N_BXs"},
+    {"title": "All About That Bass", "artist": "Meghan Trainor", "youtube_id": "7PCkvCPvDXk"},
+    {"title": "Shake It Off", "artist": "Taylor Swift", "youtube_id": "nfWlot6h_JM"},
+    # 2015
+    {"title": "Uptown Funk", "artist": "Mark Ronson ft. Bruno Mars", "youtube_id": "OPf0YbXqDm0"},
+    {"title": "Thinking Out Loud", "artist": "Ed Sheeran", "youtube_id": "lp-EO5I60KA"},
+    {"title": "Where Are Ü Now", "artist": "Jack Ü ft. Justin Bieber", "youtube_id": "nntGTK2Fhb0"},
+    # 2016
+    {"title": "One Dance", "artist": "Drake", "youtube_id": "iuP8kHaVTiI"},
+    {"title": "Work", "artist": "Rihanna ft. Drake", "youtube_id": "HL1UzIK-flA"},
+    {"title": "This Is What You Came For", "artist": "Calvin Harris ft. Rihanna", "youtube_id": "kOkQ4T5WO9E"},
+    {"title": "Sorry", "artist": "Justin Bieber", "youtube_id": "fRh_vgS2dFE"},
+    # 2017
+    {"title": "Shape of You", "artist": "Ed Sheeran", "youtube_id": "JGwWNGJdvx8"},
+    {"title": "Rockstar", "artist": "Post Malone ft. 21 Savage", "youtube_id": "UceaB4D0jpo"},
+    {"title": "Despacito", "artist": "Luis Fonsi ft. Daddy Yankee & Justin Bieber", "youtube_id": "kJQP7kiw5Fk"},
+    # 2018
+    {"title": "God's Plan", "artist": "Drake", "youtube_id": "xpVfcZ0ZcFM"},
+    {"title": "In My Feelings", "artist": "Drake", "youtube_id": "DRS_PpOrUZ4"},
+    {"title": "Sunflower", "artist": "Post Malone & Swae Lee", "youtube_id": "ApXoWvfEYVU"},
+    # 2019
+    {"title": "Old Town Road", "artist": "Lil Nas X ft. Billy Ray Cyrus", "youtube_id": "w2Ov5jzm3j8"},
+    {"title": "Bad Guy", "artist": "Billie Eilish", "youtube_id": "DyDfgMOUjCI"},
+    {"title": "7 Rings", "artist": "Ariana Grande", "youtube_id": "QYh6mYIJG2Y"},
+    # 2020
+    {"title": "Blinding Lights", "artist": "The Weeknd", "youtube_id": "4NRXx6U8ABQ"},
+    {"title": "Rockstar", "artist": "DaBaby ft. Roddy Ricch", "youtube_id": "fNLRLiAD-Qs"},
+    {"title": "Savage Love", "artist": "Jason Derulo ft. Jawsh 685", "youtube_id": "NWKzJIr_sOQ"},
+    # 2021
+    {"title": "Levitating", "artist": "Dua Lipa", "youtube_id": "TUVcZfQe-Kw"},
+    {"title": "Butter", "artist": "BTS", "youtube_id": "WMweEpGlu_U"},
+    {"title": "Stay", "artist": "The Kid LAROI & Justin Bieber", "youtube_id": "kTJczUoc26U"},
+    {"title": "Montero (Call Me By Your Name)", "artist": "Lil Nas X", "youtube_id": "6swmTBVI83k"},
+    # 2022
+    {"title": "As It Was", "artist": "Harry Styles", "youtube_id": "H5v3kku4y6Q"},
+    {"title": "Heat Waves", "artist": "Glass Animals", "youtube_id": "mRD0-GxqHVo"},
+    {"title": "Anti-Hero", "artist": "Taylor Swift", "youtube_id": "b1kbLwvqugk"},
+    # 2023
+    {"title": "Flowers", "artist": "Miley Cyrus", "youtube_id": "G7KNmW9a75Y"},
+    {"title": "Last Night", "artist": "Morgan Wallen", "youtube_id": "iyXKnEJDqFU"},
+    {"title": "Kill Bill", "artist": "SZA", "youtube_id": "2vRS1sDzdCc"},
+    {"title": "Cruel Summer", "artist": "Taylor Swift", "youtube_id": "ic8j13piAhQ"},
+    # 2024-2025
+    {"title": "Espresso", "artist": "Sabrina Carpenter", "youtube_id": "eVli-tstM5E"},
+    {"title": "Die With a Smile", "artist": "Lady Gaga & Bruno Mars", "youtube_id": "kPa7bsKwL-c"},
+    {"title": "A Bar Song (Tipsy)", "artist": "Shaboozey", "youtube_id": "GxGPEebwS8E"},
+    {"title": "Please Please Please", "artist": "Sabrina Carpenter", "youtube_id": "yHAkJFQAeAY"},
+    # ===== BANDES SON EMBLÉMATIQUES =====
+    {"title": "Lose Yourself", "artist": "Eminem", "youtube_id": "xFYQQPAOz7Y"},
+    {"title": "Eye of the Tiger", "artist": "Survivor", "youtube_id": "btPJPFnesV4"},
+    {"title": "Bohemian Rhapsody", "artist": "Queen", "youtube_id": "fJ9rUzIMcZQ"},
+    {"title": "Smells Like Teen Spirit", "artist": "Nirvana", "youtube_id": "hTWKbfoikeg"},
+    {"title": "Mr. Brightside", "artist": "The Killers", "youtube_id": "gGdGFtwCNBE"},
+]
