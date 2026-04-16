a29e346e07300a5488fc28b6cf4c8f18dfd792f9
AMSChopper <theo.buignet2@gmail.com>
2026-04-16 23:23:46 +0200

    Add files via upload

---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom' // Import important
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* On emballe l'application ici */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
